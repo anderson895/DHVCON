8a9e4a09ccfe795497a08755b5b93d2fa5d220ff
JARP <105678913+anderson895@users.noreply.github.com>
2025-10-13 20:58:27 +0800

    Update index.php

--- home/index.php
+++ home/index.php
@@ -24,7 +24,7 @@ include "../src/components/home/header.php";
    <section class="p-6 bg-gradient-to-r ">
         <div class="rounded-2xl bg-[#2b2d31] p-10 text-center shadow-lg">
             <h1 class="text-4xl font-extrabold mb-3 text-white">Welcome to DHVCON</h1>
-            <p class="text-gray-400 mb-5 text-lg">Empowering collaboration and innovation — earn your certificate through our official platform.</p>
+            <p class="text-gray-400 mb-5 text-lg">Empowering collaboration and innovation — earn your certificate through our official platform...</p>
             <div class="flex justify-center gap-4">
             <button id="btnCreateRoom" class="cursor-pointer bg-white text-black font-semibold px-5 py-2.5 rounded-md hover:bg-gray-200 transition">
                 Create Room
